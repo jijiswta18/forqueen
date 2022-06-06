6dda41622af11842a3e5409805c95d7bc0588602
jeedev <jijiswta18@gmail.com>
2022-06-06 16:17:25 +0700

    edit delete type file

--- pages/signingTotal306.vue
+++ pages/signingTotal306.vue
@@ -23,7 +23,7 @@
             :data="desserts"
             :fields="json_fields"
             worksheet="My forqueen"
-            name="forqueen.xls"
+            name="forqueen"
           >
           Export Excel
         </downloadexcel>
